e19784a222ef0f3f9e4cbade38b23be4b9a64d93
Rifa Fathima <rifaxfathima05@gmail.com>
2025-10-22 12:03:24 -0700

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p research
+
+touch src/main.py__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+touch README.md
+
+echo "files created successfully"
