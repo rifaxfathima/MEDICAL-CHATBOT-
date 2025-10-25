f1778b32a83c206660f8d1548736cb30e32d7ae4
Rifa Fathima <rifaxfathima05@gmail.com>
2025-10-25 12:49:51 -0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="RIFA FATHIMA",
+    author_email="rifaxfathima05@gmail.com",
+    packages=find_packages(),
+    install_requires=[]# Dependencies are now managed in requirements.txt
+    
+)   
